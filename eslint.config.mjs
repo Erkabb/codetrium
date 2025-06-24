ffb8fe91d376866c8dfd3304100e823fa71273d5
erkabb <erkabb816@gmail.com>
2025-06-24 17:03:16 +0800

    nn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["src/generated/**/*.ts"], // Adjust if your path is different
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
