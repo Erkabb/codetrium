6d3eca1a4123d222935649796bccf462e6cd7deb
erkabb <erkabb816@gmail.com>
2025-06-25 22:36:20 +0800

    git

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    serverActions: {
-        bodySizeLimit: '1gb',
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
