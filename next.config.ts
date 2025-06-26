97b5dc796f578b1e25cdb58dc81e74461605d06a
erkabb <erkabb816@gmail.com>
2025-06-26 10:17:00 +0800

    ignore build error config added

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        typedRoutes: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
+};
 
 export default nextConfig;
